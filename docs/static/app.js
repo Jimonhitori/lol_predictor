4e92f403df90d5bcabe25f9861d123ff0c8e8fd8
Jimonhitori <jimonnft@gmail.com>
2026-06-25 13:08:38 +0900

    Default main schedule to major leagues

--- docs/static/app.js
+++ docs/static/app.js
@@ -612,7 +612,7 @@ async function loadOptions() {
   state.options = await api('/api/options');
   fillSelect('league', state.options.leagues);
   for (const id of ['top_champion','jng_champion','mid_champion','bot_champion','sup_champion']) fillSelect(id, state.options.champions);
-  $('leagueGroup').value = 'all';
+  $('leagueGroup').value = 'major';
   if ($('championMetaGroup')) $('championMetaGroup').value = 'all';
   fillTeamStandingSelect();
   setValue('league', 'LCK');
